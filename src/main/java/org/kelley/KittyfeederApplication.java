7dbebd39fab11cf951199bb3c858f1a7bfdaec57
Troy Kelley <troy.kelley@e-gineering.com>
2016-01-17 15:49:01 -0500

    Added initial spring boot files from initializer

---
+++ src/main/java/org/kelley/KittyfeederApplication.java
@@ -0,0 +1,12 @@
+package org.kelley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KittyfeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittyfeederApplication.class, args);
+	}
+}
